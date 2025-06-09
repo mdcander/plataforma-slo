f4b8c7549af683c6d07b0dc4539d5261ef6dee12
pimpsh <anderson.moreiracarvalho@gmail.com>
2025-06-09 11:49:30 -0300

    profissionais

--- packages/core/src/constants/profissionais.ts
+++ packages/core/src/constants/profissionais.ts
@@ -4,6 +4,30 @@ export const profissionais: Profissional[] = [
     {
         id: 1,
         nome: 'Walter Heisenberg White',
+        descricao: 'Dr. Bacharel em direito',
+        imagemURL: '/profissionais/profissional.webp'
+    },
+    {
+        id: 2,
+        nome: 'Jesse Pinkman',
+        descricao: 'Phd Bacharel em direito',
+        imagemURL: '/profissionais/profissional.webp'
+    },
+    {
+        id: 3,
+        nome: 'Saul Goodman',
+        descricao: 'Bacharel em direito',
+        imagemURL: '/profissionais/profissional.webp'
+    },
+    {
+        id: 4,
+        nome: 'Gustavo Fring',
+        descricao: 'Bacharel em direito',
+        imagemURL: '/profissionais/profissional.webp'
+    },
+    {
+        id: 5,
+        nome: 'Hector Salamanca',
         descricao: 'Bacharel em direito',
         imagemURL: '/profissionais/profissional.webp'
     },
